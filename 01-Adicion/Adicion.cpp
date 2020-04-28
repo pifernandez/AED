a7b39194731e81d461e3b48faee37c8acce0f676
Pilar Fernndez <pilfernandez1@gmail.com>
2020-04-28 17:36:22 -0300

    Representacion textual

---
+++ 01-Adicion/Adicion.cpp
@@ -0,0 +1,16 @@
+/*
+Pilar Fernández
+20200428
+Adición
+*/
+
+#include <iostream>
+
+int main(){
+    int a, b;
+    std::cout << "Ingrese dos números enteros\n";
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << "La suma es: ";
+    std::cout << a + b;
+}
