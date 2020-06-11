2ef984807cd13377ea6538f73250e12e79f0bebe
Pilar Fernandez <pilfernandez1@gmail.com>
2020-06-10 23:23:53 -0300

    funcion isBisiesto

--- 03-Bisiesto/isBisiesto.cpp
+++ 03-Bisiesto/isBisiesto.cpp
@@ -3,3 +3,22 @@ Pilar Fernández
 20200611
 Bisiesto
 */
+
+#include <iostream>
+#include <cassert>
+
+bool isBisiesto(int);
+
+int main(){
+    assert(isBisiesto(2020)); 
+    assert(isBisiesto(2000)); 
+    assert(not isBisiesto(1581)); 
+    assert(not isBisiesto(1923)); 
+    assert(not isBisiesto(1900)); 
+    assert(isBisiesto(1582)); 
+    assert(isBisiesto(2036)); 
+}
+
+bool isBisiesto(int x){
+    return (x%4==0 and (x%100!=0 or x%400==0) and x>=1582);
+}
